1308b52913b6cd199f523d60d028fb52b81c2ac5
DuyHuynhSC <huynhducduy135@yahoo.com>
2026-07-01 17:10:14 +0700

    sửa vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { resolve } from 'path';
 import fs from 'fs';
 import os from 'os';
